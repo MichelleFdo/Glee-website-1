331bae34a8f3e59483daf2adc41763db0bc5109f
Michelle Fernando <54815746+MichelleFdo@users.noreply.github.com>
2020-10-26 22:15:43 +0530

    final

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login</title>
+	<title>Signup</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -23,16 +23,32 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/logo.png" alt="IMG">
+					<img src="images/logo.png" alt="IMG" width =500>
 					<h2><img src="images/icons/arrow2.png" width=50px>Follow your Interests<br/>
 					<img src="images/icons/arrow2.png" width=50px>Meet new Friends<br/>
 					<img src="images/icons/arrow2.png" width=50px>Join the Community</h2>
 				</div>
+				
 
 				<form class="login100-form validate-form">
 					<span class="login100-form-title">
-						<h1 id="glow">Member Login</h1>
+						<h1 id="glow">Member Signup</h1>
 					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="firstname" placeholder="First Name">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="lastname" placeholder="Last Name">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
@@ -52,22 +68,14 @@
 					
 					<!--<div class="container-login100-form-btn">-->
 						<button class="login100-form-btn">
-							Login
+							Signup
 						</button>
 					<!--</div>-->
 
-					<div class="text-center p-t-6">
-						<span class="txt3">
-							Forgot
-						</span>
-						<a class="txt3" href="#">
-							Username / Password?
-						</a>
-					</div>
 
 					<div class="text-center p-t-2" id="link1">
-						<a class="txt2" href="signup.php">
-							Create your Account
+						<a class="txt2" href="login.php">
+							Already have an account?
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
